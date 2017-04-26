b3e0138df227ec3a32644af7bd7cf252873f29d7
marblo <marcinblochowiak@wp.pl>
2017-04-26 17:49:09 +0200

    first step

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+var a = 0
+var b = 0
+var value = (a * a) + (2 * a * b) - (b * b);
+	if (value == 0) {
+		console.log('wartość 0');
+	}
+	else if (value > 0){
+		console.log('wartość dodatnia');
+	}
+	else {
+		console.log('wartość ujemna');
+	}
